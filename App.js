886f3abf2bcbab513f871a2ea50136fd17f55b4e
gauravP002 <gp3062614@gmail.com>
2026-02-20 20:43:45 +0530

    adding style

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <View style={styles.firstCOntainer}>
       <Text>Open up App.js to start working on your app!</Text>
+      </View>
+      <View>
+      <Text style={{margin : 10, borderWidth: 2, borderBlockColor: 'red', padding: 16}}>now we separate the app</Text>
+      <Button  title="tap me"/>
       <StatusBar style="auto" />
+    </View>
     </View>
   );
 }
@@ -17,4 +23,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  firstCOntainer:{
+    margin:20,
+    backgroundColor: '#ccbcbc',
+  }
 });
